6413b1fcf7eb0c715d3703cb0c5b87d23a9e3ad9
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-08 18:02:54 -0600

    Fix all css and linter issues that broke during dropmenu add

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,26 @@
-var items = document.getElementsByClassName("menuItem")
+const items = document.getElementsByClassName('menuItem');
+const windowSize = window.matchMedia("(max-width: 1080px)")
 
-function openNav () {
-  document.getElementById("drop-menu").style.height = "85vh";
-  document.getElementById("closeside_menu").style.display = "flex";
-  document.getElementById("dropbtn").style.display = "none";
-  document.getElementById("logo").style.display = "none";
-  for (let i = 0; i < items.length; i++) {
-    items[i].style.display = "flex";
+function openNav() {
+  if (windowSize.matches) {
+    document.getElementById('drop-menu').style.height = '85vh';
+    document.getElementById('closeside_menu').style.display = 'flex';
+    document.getElementById('dropbtn').style.display = 'none';
+    document.getElementById('logo').style.display = 'none';
+    for (let i = 0; i < items.length; i += 1) {
+      items[i].style.display = 'flex';
+    }
   }
 }
 
 function closeNav() {
-  document.getElementById("drop-menu").style.height = "0";
-  document.getElementById("closeside_menu").style.display = "none";
-  document.getElementById("dropbtn").style.display = "flex";
-  document.getElementById("logo").style.display = "initial";
-  for (let j = 0; j < items.length; j++) {
-    items[j].style.display = "none";
+  if (windowSize.matches) {
+    document.getElementById('drop-menu').style.height = '0';
+    document.getElementById('closeside_menu').style.display = 'none';
+    document.getElementById('dropbtn').style.display = 'flex';
+    document.getElementById('logo').style.display = 'initial';
+    for (let j = 0; j < items.length; j += 1) {
+      items[j].style.display = 'none';
+    }
   }
 }
