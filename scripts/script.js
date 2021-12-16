1f29191075c8e128faead1a2f591e065a0e3a208
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-16 14:46:07 -0600

    Linter2

--- scripts/script.js
+++ scripts/script.js
@@ -65,7 +65,7 @@ const projectsInfo = [
   },
   {
     title: 'Leaderboard',
-    desc: "This is a website showing all the top players in a game. This repo is to practice the use of callbacks and promises, to learn how to use proper ES6 syntax, to use ES6 modules to write modular JavaScript and to practice using webpack to bundle JavaScript.",
+    desc: 'This is a website showing all the top players in a game. This repo is to practice the use of callbacks and promises, to learn how to use proper ES6 syntax, to use ES6 modules to write modular JavaScript and to practice using webpack to bundle JavaScript.',
     langs: ['HTML', 'CSS', 'Javascript', 'Webpack'],
     imgCard: 'url("./images/projects/leaderboard-card.png")',
     imgDesk: './images/projects/leaderboard-desk.png',
